2a57a0a3a3ea797b8135886c7aa26ee564cb151d
yyang <yyang@foxmail.com>
2021-05-05 15:15:41 +0800

    2024.5.4(2)

--- src/leetcode_1473.cpp
+++ src/leetcode_1473.cpp
@@ -1,107 +1,78 @@
+#define MY_DEBUG
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
 public:
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        my_houses = houses;
-        my_cost = cost;
-        my_m = m;
-        my_n = n;
-        my_target = target;
-        res = INT32_MAX;
-        dfs( 0, 0, 0 );
-        return res==INT32_MAX? -1: res ;
-    } 
-private:
-    vector<int> my_houses;
-    vector<vector<int>> my_cost;
-    int my_m, my_n, my_target;
-    int res;
-    void dfs( int loc, int blocks_cnt, int cur_cost ){
-        bool pre_existed = loc>0;
-        if( loc == my_m - 1){
-            if( my_houses[loc] == 0 ){
-                for( int i=0; i<my_n; ++i ){
-                    if( cur_cost + my_cost[loc][i] < res ){
-                        if( pre_existed ){
-                            if( i+1 != my_houses[loc-1] && blocks_cnt + 1 == my_target ){
-                                res = cur_cost + my_cost[loc][i];
-                            }
-                            else if( i+1 == my_houses[loc-1] && blocks_cnt == my_target ){
-                                res = cur_cost + my_cost[loc][i];
-                            }
-                        }
-                        else{
-                            if( blocks_cnt + 1 == my_target ){
-                                res = cur_cost + my_cost[loc][i];
-                            }
-                        }
-                    }
-                }
-            }
-            else{
-                if( pre_existed ){
-                    if( my_houses[loc] != my_houses[loc-1] && blocks_cnt + 1 == my_target ){
-                        res = cur_cost;
-                    }
-                    else if( my_houses[loc] == my_houses[loc-1] && blocks_cnt == my_target ){
-                        res = cur_cost;
-                    }
-                }
-                else{
-                    if( blocks_cnt + 1 == my_target ){
-                        res = cur_cost;
-                    }
-                }
-            }
+        const int INF = 0x7fffffff;
+        vector< vector <int> > dp( n, vector<int>(target+1, INF) );
+        vector< vector <int> > dp_pre( n, vector<int>(target+1, INF) );
+        if( houses[0] ){
+            dp_pre[houses[0]-1][1] = 0;
+            #ifdef MY_DEBUG
+            printf( "loc=%d, color=%d, target=%d, min_cost=%d\n", 0, houses[0], 1, dp_pre[houses[0]][1] );   
+            #endif
         }
         else{
-            if( my_houses[loc] == 0 ){
-                for( int i=0; i<my_n; ++i ){
-                    if( cur_cost + my_cost[loc][i] < res ){
-                        if( pre_existed ){
-                            if( i+1 != my_houses[loc-1] && blocks_cnt + 1 <= my_target ){
-                                my_houses[loc] =i+1;
-                                dfs( loc+1, blocks_cnt+1, cur_cost + my_cost[loc][i] );
-                                my_houses[loc] = 0;
-                            }
-                            else if( i+1 == my_houses[loc-1] ){
-                                my_houses[loc] = i+1;
-                                dfs( loc+1, blocks_cnt, cur_cost + my_cost[loc][i] );
-                                my_houses[loc] = 0;
-                            }
-                        }
-                        else{
-                            if( blocks_cnt + 1 <= my_target ){
-                                my_houses[loc] = i+1;
-                                dfs( loc+1, blocks_cnt+1, cur_cost + my_cost[loc][i] );
-                                my_houses[loc] = 0;
-                            }
-                        }
-                    }
-                }
-            }
-            else{
-                if( pre_existed ){
-                    if( my_houses[loc] != my_houses[loc-1] && blocks_cnt + 1 <= my_target ){
-                        dfs( loc+1, blocks_cnt+1, cur_cost );
-                    }
-                    else if( my_houses[loc] == my_houses[loc-1] ){
-                        dfs( loc+1, blocks_cnt, cur_cost );
-                    }
-                }
-                else{
-                    if( blocks_cnt + 1 <= my_target ){
-                        dfs( loc+1, blocks_cnt+1, cur_cost );
-                    }
-                }
+            for( int j=0; j<n; ++j ){
+                dp_pre[j][1] = cost[0][j];
+                #ifdef MY_DEBUG
+                printf( "loc=%d, color=%d, target=%d, min_cost=%d\n",0 ,j+1, 1, dp_pre[j][1] );
+                #endif
             }
         }
+        for( int i=1; i<m; ++i ){
+            if( houses[i] ){
+                int loc = houses[i]-1;
+                for( int k=target; k>=1; --k ){
+                    int res = dp_pre[loc][k];
+                    for( int l=0; l<n; ++l ){
+                        if( l != loc ){
+                            res = min( res, dp_pre[l][k-1] );
+                        }
+                    }
+                    dp[loc][k] = res;
+                    #ifdef MY_DEBUG
+                    printf( "loc=%d, color=%d, target=%d, min_cost=%d\n",i, loc+1, k, dp_pre[loc][k] );
+                    #endif
+                }
+                for( int j=0; j<n ; ++j ){
+                    if( j != loc ){
+                        fill( dp[j].begin(), dp[j].end(), INF );
+                    }
+                }
+            }   
+            else{
+                for( int j=0; j<n; ++j ){
+                    for( int k=target; k>=1; --k ){
+                        int res = dp_pre[j][k];
+                        for( int l=0; l<n; ++l ){
+                            if( l != j ){
+                                res = min( res, dp_pre[l][k-1] );
+                            }
+                        }
+                        if( res != INF ){
+                            res += cost[i][j];
+                        }
+                        dp[j][k] = res;
+                        #ifdef MY_DEBUG
+                        printf( "loc=%d, color=%d, target=%d, min_cost=%d\n", i, j+1, k, dp_pre[j][k] );
+                        #endif
+                    }
+                }
+            } 
+            dp_pre.swap(dp);        
+        }
+        int res=INF;
+        for( int j=0; j<n; ++j ){
+            res = min( res, dp_pre[j][target] );
+        }
+        return res == INF? -1: res;
     }
 };
 int main(){
     Solution sl;
-    vector<int> vec = {0,2,1,2,0};
+    vector<int> vec = {0,0,0,0,0};
     vector< vector<int> > vec_vec = {{1,10},{10,1},{10,1},{1,10},{5,1}};
     printf("%d\n",sl.minCost( vec, vec_vec, 5, 2, 3 ));
 }
